5f0033cc8c38fc0f79e7cc94bfeac4936268b55a
Sebastian Reimers <info@miete-admin.de>
2016-02-28 16:42:25 +0100

    add windows version resource file

---
+++ version.h
@@ -0,0 +1,27 @@
+#define MAJOR_VERSION_STR "16"
+#define MAJOR_VERSION_INT 16
+
+#define SUB_VERSION_STR "02"
+#define SUB_VERSION_INT 2
+
+#define RELEASE_NUMBER_STR "2"
+#define RELEASE_NUMBER_INT 2
+
+#define BUILD_NUMBER_STR "0" // Build number to be sure that each result could identified.
+#define BUILD_NUMBER_INT 0
+
+// Version with build number (example "1.0.3.342")
+#define FULL_VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
+
+// Version without build number (example "1.0.3")
+#define VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR
+
+#define stringOriginalFilename	"StudioLink.dll"
+#if PLATFORM_64
+#define stringFileDescription	"StudioLink Plugin (64Bit)"
+#else
+#define stringFileDescription	"StudioLink Plugin"
+#endif
+#define stringCompanyName		"IT-Service Sebastian Reimers\0"
+#define stringLegalCopyright	"2016 IT-Service Sebastian Reimers"
+#define stringLegalTrademarks	"VST is a trademark of Steinberg Media Technologies GmbH"
