60f4d080db0288e5bc8878c7acf20d58573e42cf
Sebastian Reimers <hallo@studio-link.de>
2017-03-31 17:25:18 +0200

    update version

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #define SUB_VERSION_STR "3"
 #define SUB_VERSION_INT 3
 
-#define RELEASE_NUMBER_STR "0"
-#define RELEASE_NUMBER_INT 0
+#define RELEASE_NUMBER_STR "1"
+#define RELEASE_NUMBER_INT 1
 
 #define BUILD_NUMBER_STR "0" // Build number to be sure that each result could identified.
 #define BUILD_NUMBER_INT 0
