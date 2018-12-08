b7fc3858a7dfff86415b7c2347e00d50bb6f54ce
Sebastian Reimers <hallo@studio-link.de>
2018-12-08 13:35:04 +0100

    add version placeholder

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
-#define MAJOR_VERSION_STR "17"
-#define MAJOR_VERSION_INT 17
+#define MAJOR_VERSION_STR "SLVMAJOR"
+#define MAJOR_VERSION_INT SLVMAJOR
 
-#define SUB_VERSION_STR "3"
-#define SUB_VERSION_INT 3
+#define SUB_VERSION_STR "SLVMINOR"
+#define SUB_VERSION_INT SLVMINOR
 
-#define RELEASE_NUMBER_STR "1"
-#define RELEASE_NUMBER_INT 1
+#define RELEASE_NUMBER_STR "SLVPATCH"
+#define RELEASE_NUMBER_INT SLVPATCH
 
 #define BUILD_NUMBER_STR "0" // Build number to be sure that each result could identified.
 #define BUILD_NUMBER_INT 0
@@ -23,5 +23,5 @@
 #define stringFileDescription	"StudioLink Plugin"
 #endif
 #define stringCompanyName		"IT-Service Sebastian Reimers\0"
-#define stringLegalCopyright	"2017 IT-Service Sebastian Reimers"
+#define stringLegalCopyright	"2017-2018 IT-Service Sebastian Reimers"
 #define stringLegalTrademarks	"VST is a trademark of Steinberg Media Technologies GmbH"
